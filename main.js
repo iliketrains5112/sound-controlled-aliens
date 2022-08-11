004e9706f0c8c155a95db499a70d2e5334557e59
iliketrains <84407492+iliketrains5112@users.noreply.github.com>
2022-08-11 16:41:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // https://teachablemachine.withgoogle.com/models/V6p8xLyvE/
+
+function startClassification(){
+navigator.mediaDevices.getUserMedia({audio: true}),
+classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/V6p8xLyvE/model.json', modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
